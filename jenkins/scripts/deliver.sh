8667a4b640a4745242dcd9d746e1e46680e1a28e
Phumipat Sukpimontree <phumipat_suk@exat.co.th>
2019-06-09 15:35:48 +0700

    Update Pipline

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-mvn spring-boot:run -Dserver.port=8989 &
+java -jar target/${NAME}-${VERSION}.jar --server.port=8090
+set +x
